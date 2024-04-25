3094d55195973eed72a94537e148ad1966b588d5
ohyo555 <555gywn@gamil.com>
2024-04-25 16:55:34 +0900

    초기 세팅

---
+++ src/main/java/com/koreait/exam/account_app_2024_04/AccountApp202404Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.account_app_2024_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApp202404Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AccountApp202404Application.class, args);
+  }
+
+}
